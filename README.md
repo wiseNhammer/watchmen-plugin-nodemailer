bc4bbaaf36779b2ded6e298684cd9298483d2b21
Emily Horsman <me@emilyhorsman.com>
2015-12-02 12:02:14 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# watchmen-plugin-nodemailer
+
+A plugin for [watchmen](https://github.com/iloire/watchmen) to send email
+notifications on service events via
+[nodemailer](https://github.com/andris9/Nodemailer).
+
+## Templates
+
+This plugin comes with default templates written in
+[Handlebars](http://handlebarsjs.com/)
+for new outages and service back events. Each event has a body and subject
+template.
+
+You can override templates by copying the included template folder into a new
+location and specifying this in your environment variables (see below).
+
+Body templates must go in `templates/body` and subject templates in
+`templates/subject`
+
+Their filenames should correspond to the watchmen event they're for. For
+example, `templates/body/new-outage.hbs`. The plugin will compile all templates
+when loaded and send email for any body template it loads. This means if you
+want to send an email for the `latency-warning` event, simply create a template
+at `templates/body/latency-warning.hbs` and restart watchmen.
+
+```
+{{service.name}} ({{service.url}}) is back online after the outage on {{date data.timestamp}}
+
+Cheers,
+
+Wise Owl
+```
+
+Templates are passed a `service` object and a `data` object. The `data` object
+contains the second argument of the event, usually data on the current or last
+outage. See what the events get passed
+[here: https://github.com/iloire/watchmen/blob/master/README.md#creating-your-own-plugin](https://github.com/iloire/watchmen/blob/master/README.md#creating-your-own-plugin)
+
+## Environment variables
+
+The following sample configures SMTP credentials and specifies a custom template
+directory—overriding the default included templates as explained above.
+
+```
+AUTH_NODEMAILER_USER=alerts@watchmen.bar
+AUTH_NODEMAILER_PASS=password
+AUTH_NODEMAILER_PORT=587
+AUTH_NODEMAILER_HOST=smtp.mailhost.bar
+
+MAILER_TEMPLATE_DIRECTORY=/home/watchmen/watchmen/templates
+```
---
+++ index.js
@@ -0,0 +1,127 @@
+/*jslint node: true */
+'use strict';
+
+var glob       = require('glob');
+var handlebars = require('handlebars');
+var nodemailer = require('nodemailer');
+var path       = require('path');
+var fs         = require('fs');
+require('dotenv').load({ silent: true });
+
+handlebars.registerHelper('date', function(timestamp) {
+  return new Date(timestamp).toString();
+});
+
+
+/*
+ * Load all templates into a nice object like…
+ *
+ * {
+ *   body: {
+ *     outage: $template
+ *   },
+ *   subject {
+ *     outage: $template
+ *   }
+ * }
+ */
+function get_templates(base_directory) {
+  var files     = glob.sync(path.join(base_directory, '**/*.hbs'));
+  var templates = {};
+  files.forEach(function(template_path) {
+    var parts      = path.parse(template_path);
+    var contents   = fs.readFileSync(template_path);
+    var parent_dir = parts.dir.split(path.sep).pop();
+
+    if (!(parent_dir in templates)) {
+      templates[parent_dir] = {};
+    }
+    templates[parent_dir][parts.name] = handlebars.compile(contents.toString());
+    console.log('Found ' + parent_dir + ' template for ' + parts.name);
+  });
+
+  return templates;
+}
+
+/*
+ * Check if using default template location or one defined in environment
+ */
+var p = path.join(__dirname, 'templates');
+if ('MAILER_TEMPLATE_DIRECTORY' in process.env) {
+  p = process.env.MAILER_TEMPLATE_DIRECTORY;
+  console.log('Loading templates from ' + p + ' instead of default templates.');
+}
+var templates = get_templates(p);
+
+/*
+ * Get us a mail transporter!
+ */
+var mailCredentials = {
+  port: process.env.AUTH_NODEMAILER_PORT,
+  host: process.env.AUTH_NODEMAILER_HOST,
+  auth: {
+    user: process.env.AUTH_NODEMAILER_USER,
+    pass: process.env.AUTH_NODEMAILER_PASS
+  }
+};
+
+var mailDefaults = {
+  from: process.env.AUTH_NODEMAILER_USER
+};
+
+var transporter = nodemailer.createTransport(mailCredentials, mailDefaults);
+
+/*
+ * Handle errors during email transport
+ */
+function emailError(err, info) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(info);
+}
+
+/*
+ * Handle events from watchmen! The fun stuff!
+ */
+function handleEvent(eventName) {
+  return function(service, data) {
+    // Don't bother if there's no template
+    if (!(eventName in templates.body)) {
+      return;
+    }
+
+    // Pass this stuff into the templates
+    var context = { service: service, data: data };
+
+    // Give us a template subject or default
+    var subject = '[' + eventName + ']' + ' on ' + service.name;
+    if (eventName in templates.subject) {
+      subject = templates.subject[eventName](context);
+    }
+
+    var body = templates.body[eventName](context);
+
+    transporter.sendMail({
+      to: service.alertTo,
+      subject: subject,
+      html: body
+    }, emailError);
+  };
+}
+
+/*
+ * Any event from watchmen can have a template associated with it. If there's
+ * one in templates/body/, an email will be sent to notify support teams!
+ */
+function NodemailerPlugin(watchmen) {
+  watchmen.on('latency-warning', handleEvent('latency-warning'));
+  watchmen.on('new-outage',      handleEvent('new-outage'));
+  watchmen.on('current-outage',  handleEvent('current-outage'));
+  watchmen.on('service-back',    handleEvent('service-back'));
+  watchmen.on('service-error',   handleEvent('service-error'));
+  watchmen.on('service-ok',      handleEvent('service-ok'));
+}
+
+exports = module.exports = NodemailerPlugin;
